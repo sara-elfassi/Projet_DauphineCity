a427e65adf43556d678e50157b108c4d7d0ae994
SaraA <alaouibsara@gmail.com>
2025-12-17 18:21:31 +0100

    last

--- Dauphine_City/src/solveur/hillclimbing/Main.java
+++ Dauphine_City/src/solveur/hillclimbing/Main.java
@@ -7,15 +7,15 @@ import sacADos.Objet;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args ){
 
      
         List<Objet> objets = new ArrayList<>();
-        objets.add(new Objet("Projet Sport", 30, new int[]{16, 6, 9}));
-        objets.add(new Objet("Projet Santé", 60, new int[]{10, 3, 18}));
-        objets.add(new Objet("Projet Éducation", 80, new int[]{20, 12, 50}));
+        objets.add(new Objet("Projet Sport",30, new int[]{16, 6, 9}));
+        objets.add(new Objet("Projet Santé",60, new int[]{10, 3, 18}));
+        objets.add(new Objet("Projet Éducation",80, new int[]{20, 12, 50}));
 
         int[] budgets = {80, 40, 60};
 
@@ -23,22 +23,22 @@ public class Main {
 
         Voisinage voisinage = new Voisinage(2);  
 
-        //  Paramètres du Hill Climbing
+     
         boolean plateau= true;              
-        boolean voisinsAleatoires = true;    
-        int nbIterations = 100;              
-        int nbRedemarrage = 5;               
-        int nbVoisinsAleaMax = 10;           
+        boolean voisins_aleatoires = true;    
+        int nb_iterations = 100;              
+        int nb_redemarrage = 5;               
+        int nb_voisins_alea = 10;           
 
       
-        HillCimbing hc = new HillCimbing(voisinage, plateau, voisinsAleatoires, nbIterations, nbRedemarrage, nbVoisinsAleaMax);
+        HillCimbing hc = new HillCimbing(voisinage, plateau, voisins_aleatoires, nb_iterations, nb_redemarrage, nb_voisins_alea);
 
-        // solution
+      
         List<Objet> meilleure_solution = hc.resoudre_hill(sac);
 
       
-        System.out.println("Meilleure solution trouvée :");
-        sac.afficherContenu(meilleure_solution);
-        System.out.println("Utilité totale : " + sac.utilite(meilleure_solution));
+        System.out.println(" Meilleure solution trouvée :");
+        sac.afficherContenu(meilleure_solution );
+        System.out.println(" Utilité totale: " + sac.utilite(meilleure_solution));
     }
 }
