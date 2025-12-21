1601f4f00ccc8c83d7ff695a13086ad5047853af
sara-elfassi <saraff1439@gmail.com>
2025-12-21 12:23:46 +0100

    Restauration de modifications

--- Dauphine_City/src/equipe/TypeCout.java
+++ Dauphine_City/src/equipe/TypeCout.java
@@ -1,5 +1,12 @@
 package equipe;
 
+/**
+ * Représente les différents types de coûts associés à un projet.
+ * <p>
+ * On distingue trois types de coût : économique, social et environnemental. 
+ * Chaque évaluateur est spécialisé dans un seul type de coût.
+ * </p>
+ */
 public enum TypeCout {
     ECONOMIQUE,
     SOCIAL,
